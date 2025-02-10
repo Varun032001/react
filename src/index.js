ff9b8380df31d0758dd21365b74dc1f2cb80b0c3
Varun032001 <bandivarun01@gmail.com>
2025-02-10 14:58:36 +0530

    added varun table sucees

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Tablevarun from './TableVarun';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Tablevarun />
   </React.StrictMode>
 );
 
